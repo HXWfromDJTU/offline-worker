2d1058acbf8644df4e668ca028f5081628e323d3
SwainWong <404813471@qq.com>
2019-07-03 08:46:57 +0800

    feat:Service actived

--- sw_cached_pages.js
+++ sw_cached_pages.js
@@ -1,4 +1,38 @@
+
+const cacheName = 'v2'
 // Call Install Event 
 self.addEventListener('install', (e) => {
-    console.log('Service Install');
+    console.log('Service Worker : Install')
+
+    e.waitUntil(
+        caches
+            .open(cacheName)
+            .then(cache => {
+                console.log('Service Worker: 正在缓存文件')
+            })
+            .then(() => {
+                self.skipWaiting()
+            })
+    )
 })
+
+
+// Call Activated Event 
+self.addEventListener('activated', e => {
+    console.log('Service Worker : Activated')
+
+    e.waitUntil(
+        caches.keys().then(cacheName => {
+            return Promise.all(
+                cacheName.map(cache => {
+                    if (cache !== cacheName) {
+                        console.log('Service Worker: 清理旧的缓存')
+                        return caches.delete(cache)
+                    }
+                })
+            )
+        })
+    )
+})
+
+
